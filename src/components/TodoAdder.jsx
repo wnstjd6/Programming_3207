9668599c72dee955e8ab9fd8f072b6f77494eb15
wnstjd6 <s2406@e-mirim.hs.kr>
2026-04-14 10:11:02 +0900

    TodoAdd 공백처리 추가

--- src/components/TodoAdder.jsx
+++ src/components/TodoAdder.jsx
@@ -4,6 +4,9 @@ export default function TodoAdder({addTodo}){ //const props = {addTodo: addTodo}
     const [inputTodo, setInputTodo] = useState('');
     const handleSubmit = (event)=>{
         event.preventDefault();
+        if(inputTodo.trim() === ''){ //입력값이 빈 문자열이거나 공백으로만 이루어진 경우
+            return; //아무것도 하지 않고 함수 종료
+        }
         addTodo(inputTodo); //input에 있는 text를 value를 할일 내용으로
         setInputTodo(''); // 입력 후 입력창 비우기
 
