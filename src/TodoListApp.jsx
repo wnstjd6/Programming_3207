629dcd469d79fd4d0391135148fa9bf502a51e9a
wnstjd6 <s2406@e-mirim.hs.kr>
2026-04-14 10:05:09 +0900

    TodoListApp addTodo()

--- src/TodoListApp.jsx
+++ src/TodoListApp.jsx
@@ -1,17 +1,36 @@
-import './TodoListApp.css'
-import Button from './components/Button.jsx'
-import Checkbox from './components/Checkbox.jsx'
-import TodoItemEmpty from './components/TodoItemEmpty.jsx'
-import TodoHeader from './components/TodoHeader.jsx'
-import TodoAdder from './components/TodoAdder.jsx'
-import TodoItem from './components/TodoItem.jsx'
-import TodoList from './components/TodoList.jsx'
+import { useState } from 'react';
+import './TodoListApp.css';
+import Button from './components/Button.jsx';
+import Checkbox from './components/Checkbox.jsx';
+import TodoItemEmpty from './components/TodoItemEmpty.jsx';
+import TodoHeader from './components/TodoHeader.jsx';
+import TodoAdder from './components/TodoAdder.jsx';
+import TodoItem from './components/TodoItem.jsx';
+import TodoList from './components/TodoList.jsx';
+
+class Todo{
+  constructor(id, text, isCompleted){
+    this.id = id;
+    this.text = text;
+    this.isCompleted = isCompleted;
+  
+  }
+}
 
 function TodoListApp() {
+  const [todos, setTodos] = useState([]);
+  function addTodo(text){
+    //이전 todos에 newTodo 만들어서 추가하자 -> 그것을 setTodos() 하자
+    setTodos((todos)=>[...todos,
+       new Todo(Date.now(), 
+       text, false)]);
+
+
+  }
   return (
     <div className="todo">
       <TodoHeader />
-      <TodoAdder/>
+      <TodoAdder addTodo={addTodo} />
       <TodoList/>
     </div>
   )
--- src/components/TodoAdder.jsx
+++ src/components/TodoAdder.jsx
@@ -1,8 +1,23 @@
-import Button from './Button.jsx'
-export default function TodoAdder(){
+import { useState } from 'react';
+import Button from './Button.jsx';
+export default function TodoAdder({addTodo}){ //const props = {addTodo: addTodo}
+    const [inputTodo, setInputTodo] = useState('');
+    const handleSubmit = (event)=>{
+        event.preventDefault();
+        addTodo(inputTodo); //input에 있는 text를 value를 할일 내용으로
+        setInputTodo(''); // 입력 후 입력창 비우기
+
+    }
+
     return(
-         <form className="todo__form">
-                <input type="text" className="todo__input" placeholder="할 일을 입력하세요."/>
+         <form className="todo__form" onSubmit={handleSubmit}>
+                <input 
+                type="text" 
+                className="todo__input" 
+                placeholder="할 일을 입력하세요."
+                value = {inputTodo}
+                onChange = {(event)=> setInputTodo(event.target.value)}
+                />
                 <Button type="submit" className="todo__button todo__button--add">Add</Button>
             </form>
     )
